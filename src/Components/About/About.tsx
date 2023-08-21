f354040567a54e0cd9222434fe36491af28e9624
Jason Alberto <alberto.jason@gmail.com>
2023-08-21 00:02:46 -0400

    Add greyscale styling and responsiveness

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import './About.scss';
 
 const About = () => {
-  const aboutRef = useRef<HTMLElement | null>(null);
+  const aboutRef = useRef<HTMLDivElement | null>(null);
   const [isVisible, setIsVisible] = useState<boolean>(false);
 
   useEffect(() => {
@@ -18,12 +18,17 @@ const About = () => {
 
   return (
     <div className="about-container">
-      <article ref={aboutRef} className={`${isVisible ? 'animate-fade-in' : ''}`}>
+      <div ref={aboutRef} className={`about-content ${isVisible ? 'animate-fade-in' : ''}`}>
         <h2 className="sub-title-greet">Hi, I'm Jason</h2>
         <p>From architecture to software engineering, I like creating things people can use. My leap into software development was driven by a desire to build beyond the physical realm. From bricks and mortar to lines of code, I find immense satisfaction in transforming concepts into digital reality. As a front-end developer, I draw on my architectural expertise to shape accessible user-centered applications that seamlessly combine style and function.</p>
         <br/>
         <p>When I'm not at the computer you can find me climbing on a rock or taking care of plants.</p>
-      </article>
+        <br/>
+        <p className='shrink-text'>React | Global State Management | Typescript | Javascript | HTML5 | SASS | CSS3 | Cypress | Mocha & Chai</p>
+      </div>
+      <div ref={aboutRef} className={`profile-img-container ${isVisible ? 'animate-fade-in' : ''}`}>
+      <img className="profile-img" src={`${process.env.PUBLIC_URL + './images/overlook.png'}`} />
+      </div>
     </div>
   );
 }
